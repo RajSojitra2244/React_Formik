7c3b0b9aaca7f18c9a59d61bbe1deb8cda711a1c
RajSojitra2244 <54797309+RajSojitra2244@users.noreply.github.com>
2020-12-23 22:19:43 +0530

    reg_disign

--- formiktask/src/Formik_Parts/RegistrationForm.js
+++ formiktask/src/Formik_Parts/RegistrationForm.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
 import {Field,ErrorMessage,Form,Formik} from 'formik'
-import FormikControll from './FormikControll'
+import FormikControl from './FormikControll'
 import * as Yup from 'yup'
 import {Card,Button} from 'react-bootstrap'
 import '../css/Registration.css'
 function RegistrationForm() {
  const [FormNumber, setFormNumber] = useState(1)
-    const options=[
-        {key:'Email',value:'emailmoc'},
-        {key:'Telephone',value:'telephonemoc'},
-       
-    ]
+ const deopdownoption = [
+    { key: 'Select Course', value: '' },
+    { key: 'React', value: 'React' },
+    { key: 'Redux ', value: 'Redux' },
+    { key: 'Formik ', value: 'Formik' },
+  ];
+  const checkBoxOptions = [
+    { key: 'Html', value: 'Html' },
+    { key: 'Css', value: 'Css' },
+    { key: 'JavaScript', value: 'JavaScript' },
+  ];
+    // const options=[
+    //     {key:'Email',value:'emailmoc'},
+    //     {key:'Telephone',value:'telephonemoc'},
+    // ]
   const   initialValues={
         Name:"",
         email:'',
@@ -19,22 +29,40 @@ function RegistrationForm() {
         modeOfContact:'',
         PhoneNo:'',
         Pincode:'',
+        skills:[],
+        pincode:'',
+        course:'',
+        courseDate:null
     }
   const   validationSchema=Yup.object({
         Name:Yup.string().required('Required!'),
         email:Yup.string().email('Invalid Email format').required('Required!'),
         password:Yup.string().required('Required!'),
-        confirmPassword:Yup.string().oneOf([Yup.ref('password'),''],'Password must match').required("Required!"),
+        course: Yup.string().required('Required!'),
+        confimPassword:Yup.string().oneOf([Yup.ref('password'),''],'Password must match').required("Required!"),
         modeOfContact:Yup.string().required('Required!'),
-        phone:Yup.string().required('Required!'),
+        phone:Yup.number()
+  
+        .required('A phone number is required'),
+        pincode:Yup.string().min(6).required('Required!'),
+       courseDate: Yup.date().required('Required!').nullable(),
+
+
         
  })
+ const validateSkills = values => {
+    let error
+    if(values.length === 0){
+        error = "Required!"
+    }
+    return error
+}
 
  const  onSubmit=values=>{
      console.log("form values reg..",values);
+     if(FormNumber<3){ setFormNumber(FormNumber +1)}
  }
  const nextbutton=()=>{
-     setFormNumber(FormNumber +1)
  }
     return (
         <div>
@@ -47,39 +75,39 @@ function RegistrationForm() {
                <Form>
 
 <div className="registrationMargin">
-<div class="steps-form-2">
-    <div class="steps-row-2 setup-panel-2 d-flex justify-content-between">
-        <div class="steps-step-2">
+<div className="steps-form-2">
+    <div className="steps-row-2 setup-panel-2 d-flex justify-content-between">
+        <div className="steps-step-2 first">
             <a href="#step-1" 
             type="button" 
-            class="btn btn-amber btn-circle-2 waves-effect ml-0" 
+            className="btn btn-amber btn-circle-2 waves-effect ml-0" 
             data-toggle="tooltip" 
             data-placement="top" 
-            title="Basic Information"><i class="fa fa-folder-open-o" aria-hidden="true">1</i></a>
+            title="Basic Information"><i className="fa fa-folder-open-o" aria-hidden="true">1</i></a>
         </div>
-        <div class="steps-step-2">
+        <div className="steps-step-2 second">
             <a href="#step-2" 
             type="button" 
-            class="btn btn-blue-grey btn-circle-2 waves-effect"
+            className="btn btn-blue-grey btn-circle-2 waves-effect"
              data-toggle="tooltip" 
             data-placement="top" 
-            title="Personal Data"><i class="fa fa-pencil" aria-hidden="true">2</i></a>
+            title="Personal Data"><i className="fa fa-pencil" aria-hidden="true">2</i></a>
         </div>
-        <div class="steps-step-2">
+        <div className="steps-step-2 thired">
             <a href="#step-3" 
             type="button" 
-            class="btn btn-blue-grey btn-circle-2 waves-effect" 
+            className="btn btn-blue-grey btn-circle-2 waves-effect" 
             data-toggle="tooltip" 
             data-placement="top" 
-            title="Terms and Conditions"><i class="fa fa-photo" aria-hidden="true">3</i></a>
+            title="Terms and Conditions"><i className="fa fa-photo" aria-hidden="true">3</i></a>
         </div>
-        <div class="steps-step-2">
+        <div className="steps-step-2">
             <a href="#step-4" 
             type="button" 
-            class="btn btn-blue-grey btn-circle-2 waves-effect mr-0" 
+            className="btn btn-blue-grey btn-circle-2 waves-effect mr-0" 
             data-toggle="tooltip" 
             data-placement="top" 
-            title="Finish"><i class="fa fa-check" aria-hidden="true"></i></a>
+            title="Finish"><i className="fa fa-check" aria-hidden="true"></i></a>
         </div>
     </div>
 </div>
@@ -94,83 +122,89 @@ function RegistrationForm() {
     <Card.Body>
                 {FormNumber ==1 &&  
                 <>
-                 <FormikControll
+                 <FormikControl
                     control='input'
                     type='email'
-                    lable='Name'
+                    lable='Name*'
                     name='Name'
                     />
-                <FormikControll
+                <FormikControl
                     control='input'
                     type='email'
-                    lable='Email'
+                    lable='Email*'
                     name='email'
                     />
                     
-                 <FormikControll
+                 <FormikControl
                     control='input'
-                    type='password'
-                    lable='Phone no:'
+                    type='number'
+                    lable='Phone no*'
+                    min="0"
                     name='phone'
                     />
                 
                     </>
                      }
-                      {FormNumber ==2 &&  
+         {FormNumber ==2 &&  
                 <>
-               
-                 <FormikControll
-                    control='input'
-                    type='password'
-                    lable='Confirm Password'
-                    name='confirmPassword'
+                 <FormikControl
+                     control="select"
+                     lable="Select Course*"
+                     name="course"
+                     options={deopdownoption}
                     />
+                <FormikControl
+                  control="checkbox"
+                  lable="Your skills"
+                  name="skills"
+                  validate={validateSkills}
+                  options={checkBoxOptions}
+                />
+
+             <FormikControl
+                  control="date"
+                  lable="Date*"
+                  name="courseDate"
+             />
+
                     </>
                      }
             {FormNumber ==3 &&  
                 <>
-                 <FormikControll
+                 <FormikControl
                     control='input'
                     type='password'
                     lable='Password'
                     name='password'
                     />
-                 <FormikControll
+                 <FormikControl
                     control='input'
                     type='password'
-                    lable='Confirm Password'
+                    lable='Confirm Password*'
                     name='confimPassword'
                     />
-                 <FormikControll
+                 <FormikControl
                     control='input'
-                    type='password'
-                    lable='Phone no:'
-                    name='phone'
+                    type='number'
+                    min="0"
+                    lable='Pincode*'
+                    name='pincode'
                     />
-                
                     </>
                      }
                
-
     </Card.Body>
-    <Button variant="primary" className="nextbutton" onClick={()=>{nextbutton()}}>Next</Button>
+        
+        <button 
+        type="submit" 
+        className="btn btn-primary nextbutton" 
+        onClick={()=>{onSubmit()}}>Next</button>
+
+    {/* <Button variant="primary" className="nextbutton" onClick={()=>{nextbutton()}}>Next</Button> */}
   </Card.Body>
 </Card>
 
-{/* 
-                                         <FormikControll
-                    control='radio'
-                    lable='Mode Of Contact'
-                    name='modeOfContact'
-                    options={options}
-                    />
-                    <FormikControll
-                    control='input'
-                    type="number"
-                    lable='Phone Number'
-                    name='phone'
-                    />
-                    <button type="submit" className="btn btn-primary" disabled={!formik.isValid}>Login</button> */}
+{/*  <button type="submit" className="btn btn-primary" disabled={!formik.isValid}>Login</button> */}
                     
                     </div>
                </Form>
